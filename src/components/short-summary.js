e00930dda5a307b322b23858ef4318bda14183f4
veronikasgariboldi <verca.ribol@gmail.com>
2025-04-06 23:18:31 +0200

    details

--- src/components/short-summary.js
+++ src/components/short-summary.js
@@ -6,10 +6,10 @@ function ShortSummary() {
         <div className="homepage-containers"> 
             <div className="summary-container">
                 <h1 className="summary">A Short Summary</h1>
-                <p className="summary-text">The Porter, drunk, jokes about being the gatekeeper of hell.</p>
-                <p className="summary-text">His speech includes dark humor about sinners</p>
-                <p className="summary-text">He opens the door to Macduff and Lennox, who come to wake the king.</p>
-                <p className="summary-text">Duncan’s murder is discovered, and chaos begins.</p>
+                <p className="summary-text">- The Porter, drunk, jokes about being the gatekeeper of hell.</p>
+                <p className="summary-text">- His speech includes dark humor about sinners.</p>
+                <p className="summary-text">- He opens the door to Macduff and Lennox, who come to wake the king.</p>
+                <p className="summary-text">- Duncan’s murder is discovered, and chaos begins.</p>
             </div>
         </div>
     </div>
